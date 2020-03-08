5216dd5eb04b93c80e199c7972ab513800e0e4b3
George <george@skamnos.com>
2020-03-08 22:41:07 +0200

    Fixed dependencies for spock

---
+++ src/test/groovy/FirstSpec.groovy
@@ -0,0 +1,5 @@
+package test.groovy
+
+/**
+ * Created by George on 8/3/20.
+ */
--- src/test/groovy/FirstSpecification.groovy
+++ src/test/groovy/FirstSpecification.groovy
@@ -1,4 +1,16 @@
 package test.groovy
 
-class FirstSpecification {
+import spock.lang.Specification
+
+/**
+ * Created by George on 8/3/20.
+ */
+class FirstSpecification extends Specification{
+  def "one plus one should be equals tow"(){
+      expect:
+          1+1 == 2
+
+
+
+  }
 }
